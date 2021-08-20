caf860d2ce03a6364af867eb0e6c57defa8eaa19
usskr <rei.takagi731@gmail.com>
2021-08-20 06:25:05 +0000

    [Finish] ファイルの記述・CRUDの実装

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,11 @@
 class BooksController < ApplicationController
   def index
     @book = Book.new
+    @books = Book.all
   end
 
   def show
+    @book = Book.find(params[:id])
   end
 
   def create
@@ -13,7 +15,19 @@ class BooksController < ApplicationController
   end
 
   def edit
+    @book=Book.find(params[:id])
+  end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book)
+  end
+
+  def destroy
+    book = Book.find(params[:id])
+    book.destroy
+    redirect_to books_path
   end
 
   private
