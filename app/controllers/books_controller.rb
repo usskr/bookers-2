a2e7e7e81d767b61c7a8f0a2402ff39e9282d1f6
usskr <rei.takagi731@gmail.com>
2021-08-20 05:39:25 +0000

    [Finish] bookのcreate機能の作成まで

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,10 +1,23 @@
 class BooksController < ApplicationController
   def index
+    @book = Book.new
   end
 
   def show
   end
 
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to books_path
+  end
+
   def edit
+
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
